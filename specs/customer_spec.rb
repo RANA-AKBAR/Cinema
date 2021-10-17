4b89c610d543b2fccfe5df43d94f660db2051376
rana <ranaakbar@mail.com>
2021-10-17 04:17:08 -0700

    initial

---
+++ specs/customer_spec.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../customer.rb'
+
+
+class CustomerTest < MiniTest::Test
+
+  def setup()
+    @customer1= Customer.new("Rana",32);
+    @customer2= Customer.new("Kiran", 28);
+    @customer3= Customer.new("Andy", 29);
+  end
+
+  def test_customer_has_name();
+    assert_equal("Rana", @Customer1.name)
+  end
+
+end
